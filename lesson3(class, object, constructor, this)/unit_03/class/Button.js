0d1ff4a1d691a02925458612730de3785d198288
MrSallivan <nik.sallivan@gmail.com>
2020-10-08 00:09:55 +1000

    3

--- lesson3(class, object, constructor, this)/unit_03/class/Button.js
+++ lesson3(class, object, constructor, this)/unit_03/class/Button.js
@@ -1,7 +1,12 @@
 class Button {
+	constructor(width, height, background, value) {
+		this.width = width
+		this.height = height
+		this.background = background
+		this.value = value
+	}
 
+	render() {
 
-    render() {
-
-    }
+	}
 }
