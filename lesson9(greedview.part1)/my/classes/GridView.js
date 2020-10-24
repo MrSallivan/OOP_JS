b0c1e760e0543b651306472d2d7549c3c5639e6a
MrSallivan <nik.sallivan@gmail.com>
2020-10-24 15:33:28 +1000

    9

---
+++ lesson9(greedview.part1)/my/classes/GridView.js
@@ -0,0 +1,34 @@
+class GridView {
+	/**
+	* properties
+	* @param[array] _tableClass // CSS классы оформления
+	* @param[array] data // входные данные
+_	* @param[array] _atribute //управляем, что выводим и как из выходных данных data
+	* @param[array] _element //куда выводить таблтцу
+	* @param[array] _header //заголовок таблицы
+	* @param[array] _headerClass // CSS классы заголовка
+	*/
+
+	constructor() {
+		this._header = ''
+		this._headerClass = []
+		this._tableClass = []
+		this._element = 'body'
+		this._atribute = []
+	}
+
+/**
+* Method set Header
+*/
+	set header(header) {
+
+	}
+
+/**
+ * Method for show GridViewTable
+ */
+	render() {
+
+	}
+
+}
